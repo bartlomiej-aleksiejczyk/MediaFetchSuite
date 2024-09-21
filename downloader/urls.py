6f3ff075967c30f15ff782cf3d524c86dfcb8a4e
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-09-21 17:14:54 +0200

    Refactor iomii views

--- downloader/urls.py
+++ downloader/urls.py
@@ -1,27 +1,17 @@
 from django.urls import path
 from iommi import Form, Table, Column
+from .views import TaskListView, NewTaskView
 
 from .models import DownloadTask, TaskExecutionWindow
 
 urlpatterns = [
     path(
         "tasks/",
-        Table(
-            auto__model=DownloadTask,
-        ).as_view(),
+        TaskListView().as_view(),
     ),
     path(
         "tasks/new/",
-        Form.create(
-            auto__model=DownloadTask,
-            auto__exclude=[
-                "created_at",
-                "updated_at",
-                "priority",
-                "error_message",
-                "state",
-            ],
-        ).as_view(),
+        NewTaskView().as_view(),
     ),
     path(
         "time-windows/",
--- downloader/views.py
+++ downloader/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from iommi import Form, Table, Column, Page, Action
+from .models import DownloadTask, TaskExecutionWindow
 
-# Create your views here.
+
+class TaskListView(Page):
+    new_task_button = Action(
+        display_name="Create New Task",
+        attrs__href="new/",
+    )
+
+    tasks_table = Table(auto__model=DownloadTask)
+
+
+class NewTaskView(Page):
+    go_back_button = Action(
+        display_name="Go Back",
+        attrs__href="/download/tasks/",
+    )
+    task_form = Form.create(
+        auto__model=DownloadTask,
+        auto__exclude=[
+            "created_at",
+            "updated_at",
+            "priority",
+            "error_message",
+            "state",
+        ],
+    )
